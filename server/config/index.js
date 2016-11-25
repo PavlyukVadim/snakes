73db825f961102f01ee386e14eebeea82c618586
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2016-11-25 13:28:41 +0200

    add config for mongoose

---
+++ server/config/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by amadev on 25.11.16.
+ */
+var nconf = require('nconf');
+var path = require('path');
+
+nconf.argv()
+    .env()
+    .file({ file: path.join(__dirname, 'config.json') });
+
+module.exports = nconf;
